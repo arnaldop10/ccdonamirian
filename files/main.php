18e257d31473e6f56c6f2dc0523a33ae32abf942
Xantium Technologies <xantiumtechnologies@gmail.com>
2016-11-09 10:15:14 -0400

    actualizacion del archivo main

--- files/main.php
+++ files/main.php
@@ -1,8 +1,8 @@
 <!--Main layout-->
 <main class="">
     <div class="container-fluid text-xs-right" style="height: 800px;">
-        <a class="btn btn-secondary btn-lg" id="Prueba">Primary secondary</a>
-       	<a class="btn btn-primary btn-lg" id="Prueba2">Primary button</a>
+        <!--<a class="btn btn-secondary btn-lg" id="Prueba">Primary secondary</a>
+       	<a class="btn btn-primary btn-lg" id="Prueba2">Primary button</a>-->
     	<img src="assets/img/logo/logo_main.png" class="float-xs-right" id="logo_main">
        <!--  <a class="btn btn-default btn-lg">Primary default</a>-->
     </div>
